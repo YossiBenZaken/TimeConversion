3b193fcc00e9d33d24d3c0b7afcb55b077c2da45
YossiBenZaken <yossibz@gmail.com>
2022-02-23 07:50:30 +0200

    Initial Commit

---
+++ TimeConversion/TimeConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionApp.swift
+//  TimeConversion
+//
+//  Created by Yosef Ben Zaken on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
